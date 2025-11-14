3f5ff6d2669d2c6dbc885244d6ae9e1b5da7ef27
Adam M Aptowitz <64217755+Pugga12@users.noreply.github.com>
2025-11-14 15:50:33 -0500

    Exponential Regression

--- regressions.c
+++ regressions.c
@@ -42,6 +42,38 @@ bool linReg(const int n, const double *x, const double *y, double* m, double* b,
 	return true;
 }
 
+bool expReg(const int n, const double *x, const double *y, double* a, double* cr) {
+	if (x == NULL || y == NULL || a == NULL || cr == NULL) return false;
+	double *y_log = malloc(sizeof(double)*n);
+	if (y_log == NULL) {
+		printf("malloc of size %d bytes failed", sizeof(double)*n);
+		free(y_log);
+		return false;
+	}
+	double m = 0;
+	double b = 0;
+	double r = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (y[i] <= 0) {
+			// regression fail (ln is not defined when its input <= 0)
+			free(y_log);
+			return false;
+		}
+		y_log[i] = log(y[i]);
+	}
+
+	bool result = linReg(n, x, y_log, &m, &b, &r);
+	if (!result) {
+		// linreg fail
+		free(y_log);
+		return false;
+	} 
+	*a = exp(b);
+	*cr = exp(m);
+	return true;
+}
+
 bool quadReg(const int n, const double x[], const double y[], double* a, double* b, double* c, double* r) {
 	double sumX = 0.0;
 	double sumY = 0.0;
@@ -85,9 +117,9 @@ int main() {
 	int numPoints;
 	int choice;
 
-	printf("Select a regression\n(1) LinReg(ax+b)\n(2) QuadReg(ax^2+bx+c)\n");
+	printf("Select a regression\n(1) LinReg(ax+b)\n(2) QuadReg(ax^2+bx+c)\n(3) ExpReg (A*r^x)\n");
 	scanf("%d", &choice);
-	if (choice < 1 || choice > 2) {
+	if (choice < 1 || choice > 3) {
 		printf("Invalid selection");
 		exit(1);
 	}
@@ -114,22 +146,30 @@ int main() {
 		scanf("%lf", &y_array[i]);
 	}
 
-	double a, b, c, m, r;
+	double a, b, c, r;
 	bool result = false;
-	if (choice == 1) {
-		result = linReg(numPoints, x_array, y_array, &m, &b, &r);
-		if (result) {
-			printf("y = %fx + %f\n", m, b);
-		} else {
-			printf("Not successful!");
-		}
-	} else {
-		result = quadReg(numPoints, x_array, y_array, &a, &b, &c, &r);
-		if (result) {
-			printf("y = %fx^2 + %fx + %f", a, b, c);
-		} else {
-			printf("Not successful!");
-		}
+	switch (choice) {
+		case 1:
+			result = linReg(numPoints, x_array, y_array, &a, &b, &r);
+			if (result) {
+				printf("y = %fx + %f\n", a, b);
+			} else {
+				printf("Not successful!");
+			}
+		case 2:
+			result = quadReg(numPoints, x_array, y_array, &a, &b, &c, &r);
+			if (result) {
+				printf("y = %fx^2 + %fx + %f", a, b, c);
+			} else {
+				printf("Not successful!");
+			}
+		case 3:
+			result = expReg(numPoints, x_array, y_array, &a, &b);
+			if (result) {
+				printf("y = %f * %f^x", a, b);
+			} else {
+				printf("Not successful!");
+			}
 	}
 
 	free(x_array);
